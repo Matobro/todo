cd4df96a16605912257630452ab546fb342600ec
Matobro <Harri.taberman@hotmail.com>
2025-07-07 20:35:55 +0300

    yee

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from 'react'
 
 function App() {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = 
+    {
+      id: tasks.length === 0 ? 0 : (tasks.length -1) + 1,
+      name: input,
+      completed: false,
+    };
+    setTasks([...tasks, task])
+    setInput("")
+  };
+
+  const removeTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  const completeTask = (id) => {
+    const updatedTasks = tasks.map((task) =>
+    task.id === id ? { ...task, completed: true } : task
+    );
+    setTasks(updatedTasks)
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input value={input} onChange={handleChange}/>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {tasks.map((task, key) => {
+          return(
+            <div key={task.id}>
+              {task.completed === true ? <h1 className="task-completed">{task.name}</h1> : <h1>{task.name}</h1>}
+              <button onClick={() => completeTask(task.id)}>Complete</button>
+              <button className="button-red" onClick={() => removeTask(task.id)}>X</button>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
